24171d81270e8837915ea32c9e8f34702345c71e
Samuel <samuelh08.shm@gmail.com>
2022-06-27 16:07:22 -0500

    remove console log

--- src/api/products.js
+++ src/api/products.js
@@ -1,7 +1,6 @@
 export async function getProducts() {
   const response = await fetch('https://fakestoreapi.com/products');
   const json = await response.json();
-  console.log(json);
   return json;
 }
 
