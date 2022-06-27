663750ac220d4d7c38cb2bf5952f8901f6711554
Samuel <samuelh08.shm@gmail.com>
2022-06-27 17:00:05 -0500

    Add countdown timer to Product cards

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
 
 export default function ProductCard({ title = '', image = '', onClick }) {
+  const minutes = Math.floor(Math.random() * 3) + 1;
+
+  const [[mins, secs], setTime] = useState([minutes, 0]);
+
+  const [disabled, setDisabled] = useState(false);
+
+  const tick = () => {
+    if (mins === 0 && secs === 0) {
+      setDisabled(true);
+    } else if (secs === 0) {
+      setTime([mins - 1, 59]);
+    } else {
+      setTime([mins, secs - 1]);
+    }
+  };
+
+  useEffect(() => {
+    const timerId = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerId);
+  });
+
   return (
     <>
       <Card
@@ -15,8 +36,10 @@ export default function ProductCard({ title = '', image = '', onClick }) {
         />
         <Card.Body>
           <Card.Subtitle>{title}</Card.Subtitle>
-          <Card.Text></Card.Text>
-          <Button variant="dark" onClick={onClick}>
+          <Card.Text>{`${mins.toString().padStart(2, '0')}:${secs
+            .toString()
+            .padStart(2, '0')}`}</Card.Text>
+          <Button variant="dark" onClick={onClick} disabled={disabled}>
             Go to detail
           </Button>
         </Card.Body>
