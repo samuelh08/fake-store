db5887f929ac3dd69ed0ef840fcbd9a03b9cd2a4
Samuel <samuelh08.shm@gmail.com>
2022-06-27 16:06:10 -0500

    add loading

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
+
 import { getProducts } from '../api/products';
 import ProductCard from '../components/ProductCard';
 
 export default function Home() {
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   async function loadProducts() {
+    setLoading(true);
     const json = await getProducts();
     setProducts(json);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -19,6 +24,25 @@ export default function Home() {
   function onClick(event, id) {
     event.stopPropagation();
     navigate(`/detail/${id}`);
+  }
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          /* bring your own prefixes */
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <Spinner animation="border" role="status"></Spinner>
+      </div>
+    );
   }
 
   return (
