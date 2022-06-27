9750ff7082f823158a16bf8b6871ff1cfa4a8f86
Samuel <samuelh08.shm@gmail.com>
2022-06-27 16:06:48 -0500

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import NavBar from './containers/NavBar';
 
 const Home = React.lazy(() => import('./pages/Home'));
 const About = React.lazy(() => import('./pages/About'));
+const ProductDetail = React.lazy(() => import('./pages/ProductDetail'));
 
 export default function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/detail/:id" element={<ProductDetail />} />
       </Routes>
     </>
   );
